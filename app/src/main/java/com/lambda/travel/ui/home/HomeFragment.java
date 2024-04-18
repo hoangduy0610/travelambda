d41a77a433469ae842435c19d23da66164f9be18
Duy Nguyen Hoang <hoangduy06104@gmail.com>
2024-04-18 17:17:07 +0700

    resolve conflict

--- app/src/main/java/com/lambda/travel/ui/home/HomeFragment.java
+++ app/src/main/java/com/lambda/travel/ui/home/HomeFragment.java
@@ -10,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.lambda.travel.R;
 import com.lambda.travel.databinding.FragmentHomeScreenBinding;
 
 public class HomeFragment extends Fragment {
@@ -18,14 +19,9 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
         binding = FragmentHomeScreenBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-//        final TextView textView = binding.textHome;
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        root = inflater.inflate(R.layout.fragment_home_screen, container, false);
         return root;
     }
 
