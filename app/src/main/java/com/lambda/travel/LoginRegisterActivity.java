c4f99ff9f2a327bc608ae90e02d747cc21fed10d
Duy Nguyen Hoang <hoangduy06104@gmail.com>
2024-05-09 18:52:51 +0700

    refactor

--- app/src/main/java/com/lambda/travel/LoginRegisterActivity.java
+++ app/src/main/java/com/lambda/travel/LoginRegisterActivity.java
@@ -11,6 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.lambda.travel.ui.LoginAndRegister.LoginFragment;
 import com.lambda.travel.ui.LoginAndRegister.RegisterFragment;
 
@@ -19,6 +20,12 @@ public class LoginRegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) {
+            Intent intent = new Intent(getApplicationContext(), HomeTabActivity.class);
+            startActivity(intent);
+            finish();
+        }
         EdgeToEdge.enable(this);
         setContentView(R.layout.start_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
