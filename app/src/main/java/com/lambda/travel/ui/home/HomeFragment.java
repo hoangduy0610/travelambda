a994812f18e4fab5035e2fb07756855733bf7031
Anh Nguyễn Tiến <22520065@ms.uit.edu.vn>
2024-05-25 18:02:19 +0700

    UI seen reviews

--- app/src/main/java/com/lambda/travel/ui/home/HomeFragment.java
+++ app/src/main/java/com/lambda/travel/ui/home/HomeFragment.java
@@ -9,10 +9,13 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.lambda.travel.R;
 import com.lambda.travel.databinding.FragmentHomeScreenBinding;
+import com.lambda.travel.ui.reviews.SeenReviewFragment;
 
 public class HomeFragment extends Fragment  {
 
@@ -23,6 +26,22 @@ public class HomeFragment extends Fragment  {
         binding = FragmentHomeScreenBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         root = inflater.inflate(R.layout.fragment_home_screen, container, false);
+        View avtButton = root.findViewById(R.id.avatar);
+
+        // Set an OnClickListener to handle the onPress event
+        avtButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment seenReviewFragment = new SeenReviewFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.nav_host_fragment_activity_main,seenReviewFragment);
+                fragmentTransaction.commit();
+//                FragmentNavigator.Extras extras = new FragmentNavigator.Extras.Builder().build();
+//                NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_activity_main);
+//                navController.navigate(R.id.navigation_seenreview, null, null, extras);
+            }
+        });
         return root;
     }
 
---
+++ app/src/main/java/com/lambda/travel/ui/reviews/SeenReviewFragment.java
@@ -0,0 +1,39 @@
+package com.lambda.travel.ui.reviews;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.lambda.travel.R;
+import com.lambda.travel.databinding.FragmentHomeScreenBinding;
+import com.lambda.travel.databinding.FragmentSeenreviewsScreenBinding;
+
+public class SeenReviewFragment extends Fragment {
+
+    private FragmentSeenreviewsScreenBinding binding;
+    private BottomNavigationView bottomNav;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentSeenreviewsScreenBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        root = inflater.inflate(R.layout.fragment_seenreviews_screen, container, false);
+        bottomNav = getActivity().findViewById(R.id.nav_view);
+        bottomNav.setVisibility(View.GONE);
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        bottomNav.setVisibility(View.VISIBLE);
+        binding = null;
+    }
+}
